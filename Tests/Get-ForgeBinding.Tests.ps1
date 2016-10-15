0cd6bd713688a830f9774d237e45bec1c4fe3e55
Dominique Broeglin <dominique.broeglin@gmail.com>
2016-10-15 23:07:00 +0200

    Added proper test for Get-ForgeBinding

--- Tests/Get-ForgeBinding.Tests.ps1
+++ Tests/Get-ForgeBinding.Tests.ps1
@@ -1,9 +1,12 @@
 Set-PSDebug -Strict
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$PSScriptRoot\..\Forge\Get-ForgeContext.ps1"
 . "$PSScriptRoot\..\Forge\$sut"
 
 Describe "Get-ForgeBinding" {
-    It "should just work" {
-        # TODO: an actual test
+    It "should return the defined binding" {
+        $Script:ForgeContext = @{ Binding = @{ A = 1 }}
+        
+        (Get-ForgeBinding).A | Should Be 1
     }
 }
